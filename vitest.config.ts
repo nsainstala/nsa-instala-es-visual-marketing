5ea7329842047d4781891355882d0f68af520c7c
nsainstala <instalacoesnsa@gmail.com>
2026-03-17 18:11:05 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/nsa-instala-es-visual-marketing/",
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/test/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-  },
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
